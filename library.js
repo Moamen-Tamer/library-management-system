af391b7b1d51e72d1d5f21dea16fc89b85ea649c
Mo'men Tamer <moamenmosa288@gmail.com>
2025-09-15 21:23:41 +0300

    Update library.js

--- library.js
+++ library.js
@@ -1,5 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class Library {
-    constructor () {
+    authors;
+    genres;
+    years;
+    books;
+    dates;
+    names;
+    booksDatabase;
+    constructor() {
         this.authors = ["Alice Monroe", "David Carter", "Clara Hughes", "Mark Wilson", "Emma Blake", "Lucas Reed", "Sophia Turner"].map(author => author.toLowerCase());
         this.genres = ["Fantasy", "Thriller", "Romance", "Adventure", "Mystery", "Science Fiction"].map(genre => genre.toLowerCase());
         this.years = [2018, 2019, 2020, 2021, 2022, 2023];
@@ -7,60 +16,45 @@ class Library {
         this.dates = [];
         this.names = [];
     }
-
-    dash (character = '-') {
-        console.log(character.repeat(43));
+    dash(character = '-') {
+        console.log(character.repeat(45));
     }
-
-    /* isString(value) {
-        if (typeof value === "string") {
+    isString(value) {
+        return typeof value === 'string';
+    }
+    isNumber(value) {
+        return typeof value === 'number';
+    }
+    isStringOrNumber(value) {
+        if (this.isString(value) || this.isNumber(value)) {
             return true;
         }
-
         return false;
-    } */
-
-    isString = value => typeof value === "string";
-
-    /* isNumber(value) {
-        if (typeof value === "number") {
-            return true;
+    }
+    displayError(error) {
+        if (error instanceof Error) {
+            console.log(`error: ${error.message}`);
         }
-
-        return false;
-    } */
-
-    isNumber = value => typeof value === "number";
-
-    /* isStringOrNumber(value) {
-        if (typeof value === "string" || typeof value === "number") {
-            return true;
+        else {
+            console.log(String(error));
         }
-
-        return false;
-    } */
-
-    isStringOrNumber = value => typeof value === "string" || typeof value === "number";
-
-    async fetchBooks (key) {
+    }
+    async fetchBooks(key) {
         console.log(`searching for ${key}`);
-
         return new Promise((resolve, reject) => {
             const networkDelay = (Math.random() * 1500) + 500;
-
             setTimeout(() => {
-
                 this.booksDatabase = [
-                    { title: this.books[0],  author: this.authors[0], genre: this.genres[0], year: this.years[1] },
-                    { title: this.books[1],  author: this.authors[1], genre: this.genres[1], year: this.years[3] },
-                    { title: this.books[2],  author: this.authors[0], genre: this.genres[2], year: this.years[2] },
-                    { title: this.books[3],  author: this.authors[2], genre: this.genres[3], year: this.years[3] },
-                    { title: this.books[4],  author: this.authors[3], genre: this.genres[4], year: this.years[1] },
-                    { title: this.books[5],  author: this.authors[1], genre: this.genres[0], year: this.years[4] },
-                    { title: this.books[6],  author: this.authors[4], genre: this.genres[2], year: this.years[0] },
-                    { title: this.books[7],  author: this.authors[3], genre: this.genres[1], year: this.years[2] },
-                    { title: this.books[8],  author: this.authors[2], genre: this.genres[5], year: this.years[1] },
-                    { title: this.books[9],  author: this.authors[5], genre: this.genres[0], year: this.years[3] },
+                    { title: this.books[0], author: this.authors[0], genre: this.genres[0], year: this.years[1] },
+                    { title: this.books[1], author: this.authors[1], genre: this.genres[1], year: this.years[3] },
+                    { title: this.books[2], author: this.authors[0], genre: this.genres[2], year: this.years[2] },
+                    { title: this.books[3], author: this.authors[2], genre: this.genres[3], year: this.years[3] },
+                    { title: this.books[4], author: this.authors[3], genre: this.genres[4], year: this.years[1] },
+                    { title: this.books[5], author: this.authors[1], genre: this.genres[0], year: this.years[4] },
+                    { title: this.books[6], author: this.authors[4], genre: this.genres[2], year: this.years[0] },
+                    { title: this.books[7], author: this.authors[3], genre: this.genres[1], year: this.years[2] },
+                    { title: this.books[8], author: this.authors[2], genre: this.genres[5], year: this.years[1] },
+                    { title: this.books[9], author: this.authors[5], genre: this.genres[0], year: this.years[3] },
                     { title: this.books[10], author: this.authors[6], genre: this.genres[4], year: this.years[2] },
                     { title: this.books[11], author: this.authors[1], genre: this.genres[3], year: this.years[0] },
                     { title: this.books[12], author: this.authors[4], genre: this.genres[0], year: this.years[3] },
@@ -77,179 +71,165 @@ class Library {
                     { title: this.books[23], author: this.authors[3], genre: this.genres[2], year: this.years[3] },
                     { title: this.books[24], author: this.authors[0], genre: this.genres[1], year: this.years[2] }
                 ];
-
                 if (this.isString(key)) {
                     const keyTitle = key.toLowerCase();
-                    const found = this.booksDatabase.filter(book => book.title === keyTitle || book.author === keyTitle || book.genre === keyTitle);
-
-                    if (found.length > 0) {
-                        return resolve(found);
-                    } else {
-                        reject(`"${keyTitle}" is not found`);
+                    const foundByText = this.booksDatabase.filter(book => book.title === keyTitle || book.author === keyTitle || book.genre === keyTitle);
+                    if (foundByText.length > 0) {
+                        return resolve(foundByText);
                     }
-                } else if (this.isNumber(key)) {
+                    return reject(`"${keyTitle}" is not found`);
+                }
+                else if (this.isNumber(key)) {
                     const foundByYear = this.booksDatabase.filter(book => book.year === key);
-
                     if (foundByYear.length > 0) {
                         return resolve(foundByYear);
                     }
                     return reject(`we don't have book released in ${key}`);
-                } else {
+                }
+                else {
                     throw new Error("invalid input");
                 }
-
-            }, networkDelay)
-        })
+            }, networkDelay);
+        });
     }
-
-    async borrowBook (clientName, book) {
+    async borrowBook(clientName, book) {
         try {
             if (this.isString(clientName) && this.isString(book)) {
-                const booktitle = book.toLowerCase()
-                const wantedBook = await this.fetchBooks(booktitle);
-
+                const bookTitle = book.toLowerCase();
+                const wantedBook = await this.fetchBooks(bookTitle);
                 if (wantedBook) {
-                    if (this.dates.some(entry => entry.title === booktitle && entry.client === clientName)) {
+                    if (this.dates.some(entry => entry.title === bookTitle && entry.client === clientName)) {
                         throw new Error("This book is already borrowed");
                     }
-                        
-                    console.log(`${booktitle} borrowed successfully ...`);
+                    console.log(`${bookTitle} borrowed successfully ...`);
                     console.log(`you can have it for 3 days from now`);
-                        
-                    this.dates.push({title: wantedBook[0].title, borrowDate: Date.now()});
+                    this.dates.push({ title: wantedBook[0].title, client: clientName, borrowDate: Date.now() });
                     if (!this.names.some(name => name.client === clientName)) {
-                        this.names.push({client: clientName, penalty: 0});
+                        this.names.push({ client: clientName, penalty: 0 });
                     }
-
                     return wantedBook;
-                } else {
+                }
+                else {
                     throw new Error(`"${wantedBook}" is not found`);
                 }
-            } else {
+            }
+            else {
                 throw new Error("invalid input");
             }
-        } catch (error) {
-            console.log(`error: ${error.message}`);
+        }
+        catch (error) {
+            this.displayError(error);
             return null;
         }
     }
-
-    async returnBook (clientName, book) {
+    async returnBook(clientName, book) {
         try {
             if (this.isString(clientName) && this.isString(book)) {
-                const booktitle = book.toLowerCase();
-                const checkBorrow = this.dates.find(book => book.title === booktitle);
-
+                const bookTitle = book.toLowerCase();
+                const checkBorrow = this.dates.find(item => item.title === bookTitle && item.client === clientName);
                 if (checkBorrow) {
                     const now = Date.now();
                     const threeDays = 3 * 24 * 60 * 60 * 1000;
-
+                    this.dates = this.dates.filter(book => book.title !== bookTitle);
                     if ((now - checkBorrow.borrowDate) <= threeDays) {
-                        this.dates = this.dates.filter(book => book.title !== booktitle);
-                        return `${booktitle} returned successfully ...`;
+                        return `${bookTitle} returned successfully ...`;
                     }
-
-                    this.dates = this.dates.filter(book => book.title !== booktitle);
-
                     const penaltyFees = 10;
-                    console.log(`Book returned but you exceeded the 3 days, you need to pay $${penaltyFees}`)
-
+                    console.log(`Book returned but you exceeded the 3 days, you need to pay $${penaltyFees}`);
                     const index = this.names.findIndex(name => name.client === clientName);
-
                     if (index >= 0) {
                         this.names[index].penalty += penaltyFees;
-                    } else {
-                        throw new Error("could't find your data");
+                        return `$${penaltyFees} penalty added`;
                     }
-                } else {
+                    else {
+                        throw new Error("couldn't find your data");
+                    }
+                }
+                else {
                     return "you didn't borrow that book";
                 }
-            } else {
+            }
+            else {
                 throw new Error("invalid input");
             }
-        } catch (error) {
-            console.log(`error: ${error.message}`);
+        }
+        catch (error) {
+            this.displayError(error);
             return null;
         }
     }
-
-    async payPenalty (clientName) {
+    async payPenalty(clientName) {
         try {
             if (this.isString(clientName)) {
                 const checkName = this.names.find(name => name.client === clientName);
-
                 if (checkName) {
                     checkName.penalty = 0;
                     return `penalty paid succesfully ...`;
                 }
-
                 return `there's no penalty for "${clientName}"`;
-            } else {
+            }
+            else {
                 throw new Error("invalid input");
             }
-        } catch (error) {
-            console.log(`error: ${error.message}`);
+        }
+        catch (error) {
+            this.displayError(error);
             return null;
         }
     }
-
-    async singleSearch (key) {
+    async singleSearch(key) {
         try {
             if (this.isStringOrNumber(key)) {
                 const result = await this.fetchBooks(key);
                 return result;
-            } else {
+            }
+            else {
                 throw new Error("invalid input");
             }
-        } catch (error) {
-            console.log(`error: ${error.message}`);
-            return null
+        }
+        catch (error) {
+            this.displayError(error);
+            return null;
         }
     }
-
-    async multiSearch (keys) {
+    async multiSearch(keys) {
         try {
-
             if (!Array.isArray(keys)) {
-                throw new Error("keys must be an array");
+                throw new Error("invalid input");
             }
-
             const allStrings = keys.every(item => typeof item === "string");
             const allNumbers = keys.every(item => typeof item === "number");
             const allSameType = allStrings || allNumbers;
-
             if (!allSameType) {
                 throw new Error("all keys must be the same type (string or number)");
             }
-
             const keysPromises = keys.map(key => this.singleSearch(key));
             const results = await Promise.allSettled(keysPromises);
             return results;
-
-        } catch (error) {
-            console.log(`error: ${error.message}`);
+        }
+        catch (error) {
+            this.displayError(error);
             return null;
         }
     }
-
-    async displayBooks (filterFn = null) {
+    async displayBooks(filterFn = null) {
         try {
-            if (this.booksDatabase && this.booksDatabase.length !== 0) {
-
+            if (this.booksDatabase && this.booksDatabase.length > 0) {
                 const list = filterFn ? this.booksDatabase.filter(filterFn) : this.booksDatabase;
-
                 list.forEach(book => {
                     console.log(`title: ${book.title}\nauthor: ${book.author}\ngenre: ${book.genre}\nyear: ${book.year}`);
-                    this.dash('-');
+                    this.dash();
                 });
-
-                return this.booksDatabase;
-            } else {
+                return list;
+            }
+            else {
                 throw new Error("we don't have books at the moment");
             }
-        } catch (error) {
-            console.log(`error: ${error.message}`);;
+        }
+        catch (error) {
+            this.displayError(error);
             return null;
         }
     }
-};
+}
+//# sourceMappingURL=type.js.map
